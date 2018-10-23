038515ea466f24c70ca6302bc55a5e25c51c2859
Tim Besard <tim.besard@gmail.com>
2018-10-23 13:04:45 +0200

    Add some adaptors from CUDAnative and CuArrays.

--- src/base.jl
+++ src/base.jl
@@ -6,9 +6,22 @@ adapt_structure(A::AbstractAdaptor, xs::Tuple) = Tuple(adapt(A, x) for x in xs)
 @generated adapt_structure(A::AbstractAdaptor, x::NamedTuple) =
     Expr(:tuple, (:($f=adapt(A, x.$f)) for f in fieldnames(x))...)
 
+adapt(A::AbstractAdaptor, x::SubArray) = SubArray(adapt(A, parent(x)), parentindices(x))
+
 
 ## LinearAlgebra
 
 import LinearAlgebra: Adjoint, Transpose
 adapt_structure(A::AbstractAdaptor, x::Adjoint)   = Adjoint(adapt(A, parent(x)))
 adapt_structure(A::AbstractAdaptor, x::Transpose) = Transpose(adapt(A, parent(x)))
+
+
+## Broadcast
+
+import Base.Broadcast: Broadcasted, Extruded
+
+adapt_structure(A::AbstractAdaptor, bc::Broadcasted{Style}) where Style =
+  Broadcasted{Style}(bc.f, map(arg->adapt(A, arg), bc.args), bc.axes)
+
+adapt_structure(A::AbstractAdaptor, ex::Extruded) =
+    Extruded(adapt(A, ex.x), ex.keeps, ex.defaults)
