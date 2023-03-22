c0cb4c8b06272e472ed6c51a795b7547cc824225
Tim Besard <tim.besard@gmail.com>
2023-03-22 13:27:44 +0100

    Don't adapt opaque closures

--- src/base.jl
+++ src/base.jl
@@ -35,6 +35,11 @@ end
 
 adapt_structure(to, x::Core.Box) = Core.Box(adapt(to, x.contents))
 
+if VERSION >= v"1.7"
+  # we can't rewrite opaque closures
+  adapt_structure(to, oc::Core.OpaqueClosure) = oc
+end
+
 
 ## Broadcast
 
