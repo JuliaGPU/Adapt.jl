be4a7fd22ed346b8d62f76bc698a371b31b62269
Charles Kawczynski <kawczynski.charles@gmail.com>
2024-03-14 11:31:22 -0400

    Specialize adapt_structure for small tuples

--- src/base.jl
+++ src/base.jl
@@ -1,7 +1,18 @@
 # predefined adaptors for working with types from the Julia standard library
 
-adapt_structure(to, xs::Union{Tuple,NamedTuple}) = map(adapt(to), xs)
-
+# Use recursion to avoid inference bail-out in `map`
+#adapt_structure(to, xs::Union{Tuple,NamedTuple}) = map(adapt(to), xs)
+adapt_structure(to, xs::NamedTuple) = map(adapt(to), xs)
+adapt_structure(to, xs::Tuple) = map(adapt(to), xs)
+adapt_structure(to, xs::NTuple{N}) where {N} = map(adapt(to), xs)
+# Specialize on small Tuples
+for N in 1:10
+  @eval adapt_structure(to, xs::NTuple{$N}) = _adapt_tuple_structure(to, xs)
+end
+_adapt_tuple_structure(to, xs::Tuple) =
+  (adapt(to, first(xs)), _adapt_tuple_structure(to, Base.tail(xs))...)
+_adapt_tuple_structure(to, xs::Tuple{}) = ()
+_adapt_tuple_structure(to, xs::Tuple{<:Any}) = (adapt(to, first(xs)), )
 
 ## Closures
 
